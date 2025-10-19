5da510bb9a35b87f8be05bf78476320d44c427a1
Perepadin <alyansalex@gmail.com>
2025-10-19 11:27:22 +0300

    first commit

---
+++ utils/gttp_method.py
@@ -0,0 +1,23 @@
+import requests
+
+"""Список HTTP методов"""
+class Http_method:
+    headers = {'Contetn-Tyep': 'application/json'}
+    cookie = ''
+
+    @staticmethod
+    def custom_get_method(url):
+        result = requests.get(url, headers=Http_method.headers, cookie=Http_method.cookie)
+        return result
+
+    def custom_post_method(url,body):
+        result = requests.post(url, json=body, headers=Http_method.headers, cookie=Http_method.cookie)
+        return result
+
+    def custom_put_method(url,body):
+        result = requests.put(url, json=body, headers=Http_method.headers, cookie=Http_method.cookie)
+        return result
+
+    def custom_delete_method(url,body):
+        result = requests.delete(url, json=body, headers=Http_method.headers, cookie=Http_method.cookie)
+        return result
